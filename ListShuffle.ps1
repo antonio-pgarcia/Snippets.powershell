5e962b2b9c2327224b3e02cd3ed1835f7f0a3c6a
Antonio Prestes García <antonio.garcia2@gmail.com>
2015-03-06 12:09:08 +0100

    Update ListShuffle.ps1

--- ListShuffle.ps1
+++ ListShuffle.ps1
@@ -10,6 +10,11 @@ function tail($list) {
 	return $list | Select-Object -skip 1
 }
 
+# -- | Function length
+function length($list) {
+	return $list.length
+}
+
 # -- | Function myrandom
 function myrandom($a, $b, $s) {
 	return Get-Random -Minimum $a -Maximum $b -SetSeed $s
