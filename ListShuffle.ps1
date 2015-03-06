0ad5d0f5236da416800a7ed91d913787cbc95f9e
Antonio Prestes García <antonio.garcia2@gmail.com>
2015-03-06 11:12:41 +0100

    Create ListShuffle.ps1

---
+++ ListShuffle.ps1
@@ -0,0 +1,19 @@
+using System;
+
+# -- | Function head
+function head($list) {
+	return $list | Select-Object -first 1
+}
+
+# -- | Function tail
+function tail($list) {
+	return $list | Select-Object -skip 1
+}
+
+# -- | The main entry point.
+function main {
+    Console.Write("Welcome to ListShuffle!");
+}
+
+
+main
